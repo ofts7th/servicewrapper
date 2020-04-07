cf2058a9ab29c971c83b6bdcdb6e78bcedd460fb
sunliang <sunliang27@qq.com>
2020-04-07 08:07:51 +0800

    add files

---
+++ servicewrapper/Service1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace serviceWrapper
+{
+    public partial class Service1 : ServiceBase
+    {
+        Process process = null;
+        string exePath = null;
+        string processName = null;
+        string identifidBy = null;
+        string pidFile = null;
+
+        public Service1()
+        {
+            InitializeComponent();
+            exePath = ConfigurationManager.AppSettings["exePath"];
+            processName = Path.GetFileNameWithoutExtension(exePath);
+            identifidBy = ConfigurationManager.AppSettings["identifidBy"];
+            pidFile = ConfigurationManager.AppSettings["pidFile"];
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            process = callExe(exePath, ConfigurationManager.AppSettings["startPara"]);
+            process.Start();
+        }
+
+        protected override void OnStop()
+        {
+            if (identifidBy == "name")
+            {
+                if (string.IsNullOrEmpty(ConfigurationManager.AppSettings["stopPara"]))
+                {
+                    if (process != null)
+                    {
+                        process.Kill();
+                        process = null;
+                    }
+                    else
+                    {
+                        foreach (var p in Process.GetProcessesByName(processName))
+                        {
+                            p.Kill();
+                        }
+                    }
+                }
+                else
+                {
+                    var p = callExe(exePath, ConfigurationManager.AppSettings["stopPara"]);
+                    p.Start();
+                    p.WaitForExit();
+                }
+            }
+            else if (identifidBy == "pid")
+            {
+                var p = Process.GetProcessById(int.Parse(File.ReadAllText(pidFile)));
+                if (p != null)
+                {
+                    p.Kill();
+                }
+            }
+        }
+
+        static Process callExe(string exepath, string para)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = exepath;
+            if (!string.IsNullOrEmpty(para))
+            {
+                p.StartInfo.Arguments = para;
+            }
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.WorkingDirectory = exepath.Substring(0, exepath.LastIndexOf('\\'));
+            return p;
+        }
+    }
+}
---
+++ springbootwrapper/Service1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace serviceWrapper
+{
+    public partial class Service1 : ServiceBase
+    {
+        public void logMessage(string m)
+        {
+            if ("log" == ConfigurationManager.AppSettings["log"])
+            {
+                m = DateTime.Now.ToString("[yyyy-MM-dd HH:mm:ss]") + m + Environment.NewLine;
+                File.AppendAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.txt"), m, Encoding.UTF8);
+            }
+        }
+
+        string javaPath = ConfigurationManager.AppSettings["javaPath"];
+        string appName = ConfigurationManager.AppSettings["appName"];
+        string classPath = ConfigurationManager.AppSettings["classPath"];
+        string jarDir = ConfigurationManager.AppSettings["jarDir"];
+        string pidFile = ConfigurationManager.AppSettings["pidFile"];
+        string javaArgs = "";
+
+        public Service1()
+        {
+            InitializeComponent();
+            List<string> cpList = new List<string>();
+            cpList.Add(classPath);
+            foreach (string jar in Directory.GetFiles(jarDir))
+            {
+                if (jar.EndsWith(".jar"))
+                {
+                    cpList.Add(jar);
+                }
+            }
+            cpList.RemoveAll(x => string.IsNullOrEmpty(x));
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < cpList.Count; i++)
+            {
+                if (i > 0)
+                {
+                    sb.Append(";");
+                }
+                sb.Append(cpList[i]);
+            }
+            sb.Append(" ");
+            javaArgs = " -classpath " + sb.ToString() + " " + appName;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            callExe(javaPath, javaArgs).Start();
+        }
+
+        protected override void OnStop()
+        {
+            Process.GetProcessById(int.Parse(File.ReadAllText(pidFile))).Kill();
+        }
+
+        static Process callExe(string exepath, string para)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = exepath;
+            if (!string.IsNullOrEmpty(para))
+            {
+                p.StartInfo.Arguments = para;
+            }
+            p.StartInfo.UseShellExecute = false;
+            return p;
+        }
+    }
+}
